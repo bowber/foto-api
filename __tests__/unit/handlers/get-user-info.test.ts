98eb5c0ca2e35e3130d9898332f4344fd66651b4
bowber <baolam.9a9@gmail.com>
2022-11-14 16:23:38 +0700

    Use simple syntax for test

--- __tests__/unit/handlers/get-user-info.test.ts
+++ __tests__/unit/handlers/get-user-info.test.ts
@@ -1,10 +1,9 @@
-import {handler} from '../../../src/handlers/get-user-info';
-import {APIGatewayProxyEvent} from 'aws-lambda';
-
+import { handler } from '../../../src/handlers/get-user-info';
 describe('get-user-info', () => {
     it('should return a 200 response', async () => {
-        const event = {} as APIGatewayProxyEvent;
-        const result = await handler(event, null, null) as {statusCode: number};
+        const event = {} as any;
+        const result = await handler(event, null, null) as any;
         expect(result.statusCode).toEqual(200);
     });
+    
 });
